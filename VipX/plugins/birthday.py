881b63755ecb5f4f786f7c7b9059e4e24fd5da65
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-23 08:52:14 -1100

    Create birthday.py

---
+++ VipX/plugins/birthday.py
@@ -0,0 +1,19 @@
+import Vipx
+from Vipx.modules.utils
+
+# Initialize your bot with the Telegram Bot Token
+bot = telebot.TeleBot('YOUR_TELEGRAM_BOT_TOKEN')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! How can I assist you?")
+
+@bot.message_handler(func=lambda message: True)
+def send_birthday_wish(message):
+    if message.text.lower() == "/wish":
+        bot.reply_to(message, "Happy birthday! 🎉🎂🎈")
+    else:
+        bot.reply_to(message, "Sorry, I don't understand that command. Type /wish to get a birthday wish!")
+
+# Start the bot
+bot.polling()
