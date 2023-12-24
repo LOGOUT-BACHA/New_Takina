40119f7050bd4a0dd93844055181a8f2ed7ee87f
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-24 23:24:49 +0530

    afk.py

--- VipX/plugins/afk.py
+++ VipX/plugins/afk.py
@@ -1,21 +1,37 @@
 
-from VipX.ext import Updater, CommandHandler, MessageHandler, Filters
+from VipX.ext import Updater,CommandHandler,MessageHandler,Filters
 
-# AFK message handler
-def afk_message(update, context):
-    name = update.effective_user.first_name
-    update.message.reply_text(f"Hello {name}, I'm currently not available. I'll get back to you soon.")
+# Function to handle the "/afk" command
+def set_afk(update, context):
+    user = update.message.from_user
+    context.user_data['afk'] = True
+    update.message.reply_text(f"{message.from_user.mention} is now AFK.")
 
-def main():
-    TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+# Function to handle all incoming messages
+def handle_messages(update, context):
+    if 'afk' in context.user_data and context.user_data['afk']:
+        user = update.message.from_user
+        update.message.reply_text(f"Sorry, {message.from_user.mention} is currently AFK.")
 
-    # Register the AFK message handler
-    dp.add_handler(MessageHandler(Filters.text, afk_message))
+# Function to handle the "/back" command
+def set_back(update, context):
+    user = update.message.from_user
+    if 'afk' in context.user_data and context.user_data['afk']:
+        del context.user_data['afk']
+        update.message.reply_text(f"Welcome back, {message.from_user.mention}!")
 
-    updater.start_polling()
-    updater.idle()
+# Create an instance of the Updater class
+updater = Updater('YOUR_TELEGRAM_BOT_TOKEN', use_context=True)
 
-if __name__ == '__main__':
-    main()
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# Register command handlers
+dispatcher.add_handler(CommandHandler('afk', set_afk))
+dispatcher.add_handler(CommandHandler('back', set_back))
+
+# Register message handler
+dispatcher.add_handler(MessageHandler(Filters.text, handle_messages))
+
+# Start polling for updates from Telegram
+updater.start_polling()
