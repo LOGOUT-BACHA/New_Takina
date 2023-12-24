73bf04b2677269072e974d48ce106ac9bf432476
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-24 11:03:47 +0530

    birthday.py

--- VipX/plugins/birthday.py
+++ VipX/plugins/birthday.py
@@ -11,24 +11,9 @@ from strings import get_command
 from VipX.misc import SUDOERS
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-            
-WISH = [ '''
- ✧✧🔥🔥🔥🔥🔥✧✧
- ✧╭┻┻┻┻┻┻┻┻┻┻╮✧
- ✧┃╱╲ 🍓╱╲🍒 ╱╲┃✧
- ╭┻━🍒━━━🍍━━━┻╮
- ┃╱╲╱╲ 🍈╱╲🍇 ╱╲ ┃
- 🎁━━━━━━━━━━━━🎁
- ✨💕Happy Birthday💕✨''' ,
-'''
- 🌹🌹🌹🌹🌹🌹🌹
- 🌹✨✨🎀✨✨🌹
- 🌹🌟🎁🙆🎁🌟🌹
- 🌹🌟🎁💎🎁🌟🌹
- 🌹🌟🎁💖🎁🌟🌹
- 🌹 Happy bday🌹
- 🌹🌹🌹🌹🌹🌹🌹''' ,
-]  
+WISH = [ "✧═══════•❁❀❁•═══════✧/n🎂ʜᴀᴘᴘʏ ʙɪʀᴛʜᴅᴀʏ ɢᴏᴠɪɴᴅ ʙᴀʜɪ ɢᴏᴅ ʙʟᴇss ʏᴏᴜ✨/nsᴛᴀʏ ᴀʟᴡᴀʏs ʜᴀᴘᴘʏ✨/n🥳ᴇɴᴊᴏʏ ʏᴏᴜʀ ʙᴇᴀᴜᴛɪғᴜʟ ᴅᴀʏ🥳/n╭─────── • ◆ • ───────╮/n      🎂𝐇𝐀𝐏𝐏𝐘 𝐁𝐈𝐑𝐓𝐇𝐃𝐀𝐘🎂/n╰─────── • ◆ • ───────╯/n🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂/n✧═══════•❁❀❁•═══════✧",
+
+]
 
 
 @app.on_message(
@@ -47,4 +32,4 @@ async def help(client: Client, message: Message):
     & filters.private
     & ~filters.edited & filters.private & ~filters.edited)
 async def help(client: Client, message: Message):
-    await message.reply_text("**ᴡɪsʜ ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋ ᴏɴ ɢʀᴏᴜᴘ >> /wish ᴄᴏᴍᴍᴀɴᴅ.**")        
+    await message.reply_text("**𝐖𝐢𝐬𝐡 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐖𝐨𝐫𝐤 𝐨𝐧 𝐠𝐫𝐨𝐮𝐩 >> /wish 𝐂𝐨𝐦𝐦𝐚𝐧𝐝.**")
