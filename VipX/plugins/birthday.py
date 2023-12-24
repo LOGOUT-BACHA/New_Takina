2b61251c948c953a1fe2a7501518560538257f83
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-24 10:40:50 +0530

    birthday.py

--- VipX/plugins/birthday.py
+++ VipX/plugins/birthday.py
@@ -1,18 +1,53 @@
-import telebot
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from VipX import app
+import string
+from strings import get_command
+from VipX.misc import SUDOERS
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Initialize your bot with the Telegram Bot Token
-bot = telebot.TeleBot('YOUR_TELEGRAM_BOT_TOKEN')
+@AdminActual
+async def birthday(client, message: Message, _):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+    await save_authuser(message.chat.id, token, assis)
+    return await message.reply_text("**» ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴏɴ ᴜsᴇʀ ᴍᴇssᴀɢᴇ.**") 
+            
+WISH = [ '''
+✧✧🔥🔥🔥🔥🔥✧✧
+✧╭┻┻┻┻┻┻┻┻┻┻╮✧
+✧┃╱╲ 🍓╱╲🍒 ╱╲┃✧
+╭┻━🍒━━━🍍━━━┻╮
+┃╱╲╱╲ 🍈╱╲🍇 ╱╲ ┃
+🎁━━━━━━━━━━━━🎁
+✨💕Happy Birthday💕✨''' ,
+    '''
+🌹🌹🌹🌹🌹🌹🌹
+🌹✨✨🎀✨✨🌹
+🌹🌟🎁🙆🎁🌟🌹
+🌹🌟🎁💎🎁🌟🌹
+🌹🌟🎁💖🎁🌟🌹
+🌹 Happy bday🌹
+🌹🌹🌹🌹🌹🌹🌹''' ,
+@app.on_message(
+    filters.command("wish")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        text = random.choice(GALI),
+        
+    )
 
-@app.on_message(filters.command("start"=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello! How can I assist you?")
 
-@app.on_message(filters.command("wish") lambda message: True)
-def send_birthday_wish(message):
-    if message.text.lower() == "/wish":
-        bot.reply_to(message, "Happy birthday! 🎉🎂🎈")
-    else:
-        bot.reply_to(message, "Sorry, I don't understand that command. Type /wish to get a birthday wish!")
-
-# Start the bot
-bot.("Vipx")
+@app.on_message(
+    filters.command("wish")
+    & filters.private
+    & ~filters.edited & filters.private & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text("**𝐆𝐚𝐥𝐢 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐖𝐨𝐫𝐤 𝐨𝐧 𝐠𝐫𝐨𝐮𝐩 >> /gali 𝐂𝐨𝐦𝐦𝐚𝐧𝐝.**")        
