202d4f0de418405b27e5ab1434de91042f54f108
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-12-24 08:27:55 +0530

    Update birthday.py

--- VipX/plugins/birthday.py
+++ VipX/plugins/birthday.py
@@ -3,11 +3,11 @@ import telebot
 # Initialize your bot with the Telegram Bot Token
 bot = telebot.TeleBot('YOUR_TELEGRAM_BOT_TOKEN')
 
-@app.message(filters.command("start"=['start', 'help'])
+@app.on_message(filters.command("start"=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Hello! How can I assist you?")
 
-@app.message(filters.command("wish") lambda message: True)
+@app.on_message(filters.command("wish") lambda message: True)
 def send_birthday_wish(message):
     if message.text.lower() == "/wish":
         bot.reply_to(message, "Happy birthday! 🎉🎂🎈")
