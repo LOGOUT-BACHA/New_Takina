aad7be961151dbf99e429263b51ec4c411bb83b5
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-26 10:02:05 +0530

    welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from VipX import app  
 
-@bot.on(events.ChatAction())  # pylint:disable=E0602
+@app.on(events.ChatAction())  # pylint:disable=E0602
 async def _(event):
     cws = get_current_welcome_settings(event.chat_id)
     if cws:
