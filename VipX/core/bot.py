41880020c6f2cec4c3131866b40c295a6271164a
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-28 09:31:23 +0530

    Update bot.py

--- VipX/core/bot.py
+++ VipX/core/bot.py
@@ -2,8 +2,6 @@ import sys
 
 from pyrogram import Client
 from pyrogram.types import BotCommand
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
 
 import config
 
