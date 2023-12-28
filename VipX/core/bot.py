ffe03bfcd667be1df78a0f921a3792eae124db8d
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-28 09:17:15 +0530

    Update bot.py

--- VipX/core/bot.py
+++ VipX/core/bot.py
@@ -75,7 +75,7 @@ class VipXBot(Client):
             pass
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
-        if a.status != ChatMemberStatus."ADMINISTRATOR":    
+        if a.status != ChatMemberStatus.administrator:    
             LOGGER(__name__).error(
                 "🤐𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐈𝐬 𝐍𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐈𝐧 𝐋𝐨𝐠𝐠𝐞𝐫 𝐆𝐫𝐨𝐮𝐩, 𝐆𝐨 𝐀𝐧𝐝 𝐌𝐚𝐤𝐞 𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐎𝐭𝐡𝐞𝐫𝐰𝐢𝐬𝐞 𝐁𝐨𝐭 𝐖𝐢𝐥𝐥 𝐍𝐨𝐭 𝐖𝐨𝐫𝐤🤬"
             )
