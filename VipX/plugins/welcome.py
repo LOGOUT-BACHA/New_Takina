edd29c3e8f9f73d4717b68387a072b9d3b7631c7
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-30 18:34:03 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -40,22 +40,13 @@ class temp:
 
 def circle(pfp, size=(500, 500)):
     pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
-    
-    # Check if pfp has an alpha channel
-    if pfp.mode != 'RGBA':
-        pfp = pfp.convert('RGBA')
-
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + bigsize, fill=255)
     mask = mask.resize(pfp.size, Image.ANTIALIAS)
-    
-    # Check if pfp has an alpha channel before applying putalpha
-    if 'A' in pfp.getbands():
-        mask = ImageChops.darker(mask, pfp.split()[-1])
-        pfp.putalpha(mask)
-    
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
     return pfp
 
 
